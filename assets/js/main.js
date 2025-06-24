24adca55146391e126c5a3258d03bf4daab04210
pblo <pdiazarenas541@gmail.com>
2025-06-24 11:42:25 +0100

    FEATURES: js, css and html structure

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+//First Task
+
+imageCustom = document.querySelector("#image-customized-border");
+borderState = false;
+
+//Second Task
+currentStatus = document.querySelector("#current-status");
+stickerOneValue = document.querySelector("#stickerone");
+stickerTwoValue = document.querySelector("#stickertwo");
+stickerThreeValue = document.querySelector("#stickerthree");
+
+//Third Task
+passStatus = document.querySelector("#pass-status");
+passOne = document.querySelector("#pass-one");
+passTwo = document.querySelector("#pass-two");
+passThree = document.querySelector("#pass-three");
