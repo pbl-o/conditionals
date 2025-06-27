8c15e8911cfba3eb15fe7883a5242222f802cd34
pblo <pdiazarenas541@gmail.com>
2025-06-27 01:29:25 +0100

    DOC: README.md

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ imageCustom = document.querySelector("#image-customized-border");
 borderState = false;
 
 //Second Task
-currentStatus = document.querySelector("#current-status");
+currentStatus = document.querySelector("#sticker-current-status");
 stickerOneValue = document.querySelector("#stickerone");
 stickerTwoValue = document.querySelector("#stickertwo");
 stickerThreeValue = document.querySelector("#stickerthree");
@@ -14,3 +14,8 @@ passStatus = document.querySelector("#pass-status");
 passOne = document.querySelector("#pass-one");
 passTwo = document.querySelector("#pass-two");
 passThree = document.querySelector("#pass-three");
+
+
+/*Soy consciente de que sería mejor tener un archivo js o script para cada página.
+Como no hay tanto código y solo son query selectors, preferí dejarlo de esta forma
+organizado por nombres */
